b2a3f09f416f6dc0da35ba7ff2c59da52d8b157e
GlyphGryph <glyphgryph@gmail.com>
2015-09-14 21:25:08 -0400

    Based title screen

--- src/introScreen.js
+++ src/introScreen.js
@@ -1,8 +1,12 @@
 NSTC.IntroScreen = function(game){};
 NSTC.IntroScreen.prototype = {
   create: function(){
-    this.game.add.text(250, 50, "GUM FIGHTER!", { fill: '#FFF' });
-    this.game.add.text(250, 250, "Press 'Enter' to continue", { fill: '#FFF' });
+    this.title = this.game.add.text(0, 0, "GUMFIGHTER", { fill: '#FCC', fontSize: 82 });
+    this.directions = this.game.add.text(0, 0, "Press 'Enter' to continue", { fill: '#FFF' });
+    this.title.x = this.game.width/2 - this.title.width/2;
+    this.title.y = this.game.height/2 - this.title.height;
+    this.directions.x = this.game.width/2 - this.directions.width/2;
+    this.directions.y = this.title.bottom + this.directions.height;
   },
   update: function(){
     this.game.keyManager.update();
