064c6d1a6fdc7f7d414f2a66fe6a4f253f6b2a6f
GlyphGryph <glyphgryph@gmail.com>
2015-09-14 21:35:46 -0400

    Improved title screen

--- src/introScreen.js
+++ src/introScreen.js
@@ -5,8 +5,10 @@ NSTC.IntroScreen.prototype = {
     this.directions = this.game.add.text(0, 0, "Press 'Enter' to continue", { fill: '#FFF' });
     this.title.x = this.game.width/2 - this.title.width/2;
     this.title.y = this.game.height/2 - this.title.height;
+    this.title.rotation = -0.1;
     this.directions.x = this.game.width/2 - this.directions.width/2;
     this.directions.y = this.title.bottom + this.directions.height;
+    this.titleTween = this.game.add.tween(this.directions).to( { alpha: .5 }, 400, Phaser.Easing.Default, true, 0, -1, true);
   },
   update: function(){
     this.game.keyManager.update();
