4523b2abe0c572d69ce9b70bf21ea9515a86af09
GlyphGryph <glyphgryph@gmail.com>
2015-09-14 22:16:41 -0400

    Columns can now be 'joined'

--- src/selectionScreen.js
+++ src/selectionScreen.js
@@ -2,20 +2,39 @@ NSTC.SelectionScreen = function(game){};
 NSTC.SelectionScreen.prototype = {
   create: function(){
     this.columns = [
-      {color:0x9999FF},
-      {color:0x99FF99},
-      {color:0xFFFF99},
-      {color:0x99FFFF}
+      {color:0x9999FF, joinKey: "a", name: "Fred"},
+      {color:0x99FF99, joinKey: "s", name: "Tony"},
+      {color:0xFFFF99, joinKey: "w", name: "Joe"},
+      {color:0x99FFFF, joinKey: "d", name: "Albert"}
     ]
     
     var column, background;
     for(var ii=0;ii<this.columns.length;ii+=1){
       column = this.columns[ii];
-      background = new Phaser.Graphics().beginFill(column.color).drawRect(0,0,this.game.width/4,this.game.height);
-      column.background = this.game.add.sprite(this.game.width/4 * ii, 0, background.generateTexture());
+      column.state = "open";
+      column.width = this.game.width/this.columns.length
+      column.height = this.game.height;
+      column.left = column.width * ii;
+      background = new Phaser.Graphics().beginFill(column.color).drawRect(0,0,column.width,column.height);
+      column.background = this.game.add.sprite(column.left, 0, background.generateTexture());
+      column.joinText = this.game.add.text(
+        0, this.game.height/2,
+        "Press '"+column.joinKey+"' to join",
+        { fill: '#000', fontSize: 12 }
+      )
+      column.joinText.x = column.left+column.width/2-column.joinText.width/2;
     }
   },
   update: function(){
     this.game.keyManager.update();
+    var column;
+    for(var ii=0;ii<this.columns.length;ii+=1){
+      column = this.columns[ii];
+      if(column.state == "open" && this.game.keyManager.isReleased(column.joinKey)){
+        column.state = "characterSelect";
+        column.joinText.text = "Welcome, "+column.name+"!";
+        column.joinText.x = column.left+column.width/2-column.joinText.width/2;
+      }
+    }
   }
 }
