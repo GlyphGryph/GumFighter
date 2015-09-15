fffe8ded45c26720528826ab6ee11893766d7d14
GlyphGryph <glyphgryph@gmail.com>
2015-09-15 19:05:03 -0400

    Added transition from chewing to blowing, and a floatingText function

--- src/gameScreen.js
+++ src/gameScreen.js
@@ -11,6 +11,18 @@ NSTC.GameScreen.prototype = {
     graphic = new Phaser.Graphics().beginFill(0x99FF99).drawRect(0,0,this.game.width,this.game.height/2);
     this.background.ground = this.game.add.sprite(0, this.game.height/2, graphic.generateTexture());
 
+    this.messages = {
+      chewDirection: function(player){ return "Alternate '"+player.leftKey+"' and '"+player.rightKey+"' to chew!" },
+      chewStart: "Begin chewing!",
+      chewContinue: "Keep going!",
+      chewAlmostAchieved: "Almost there!",
+      chewAchieved: "Perfect!",
+      chewAlmostOver: "Woah, hold on!",
+      chewOver: "OVER TENDERIZED! :(",
+      blowDirection: function(player){ return "Hold '"+player.selectKey+"' to breathe in,\npress '"+player.leftKey+"' and '"+player.rightKey+"' to steady!" },
+      blowStart: "Blow! Blow! Blow!"
+    }
+
     var player;
     for(var ii=0;ii<this.players.length;ii+=1){
       player = this.players[ii];
@@ -25,17 +37,26 @@ NSTC.GameScreen.prototype = {
       player.optimalChewMax = 88000;
       player.bubbleSize = 0;
       player.bubbleHealth = 100;
+      player.state = "chewing";
 
       player.directionText = this.game.add.text(
         0, this.game.height/2+10,
-        "Alternate '"+player.leftKey+"' and '"+player.rightKey+"' to chew!",
-        { fill: '#000', fontSize: 12 }
+        this.messages.chewDirection(player),
+        { fill: '#000', fontSize: 12, align: 'center' }
       )
       player.directionText.x = player.left+player.width/2-player.directionText.width/2;
+
+      player.progressText = this.game.add.text(
+        0, this.game.height/2+60,
+        this.messages.chewStart,
+        { fill: '#000', fontSize: 12, align: 'center' }
+      )
+      player.progressText.x = player.left+player.width/2-player.progressText.width/2;
 
       
       player.statText = this.game.add.text(0, this.game.height/2+100,"",{ fill: '#000', fontSize: 12 })
       this.updateStatText(player);
+      this.floatText(player, "GO!");
     }
   },
   updateStatText(player){
@@ -46,18 +67,75 @@ NSTC.GameScreen.prototype = {
     player.statText.text += "Bubble Health: "+player.bubbleHealth;
     player.statText.x = player.left+player.width/2-player.statText.width/2;
   },
-  update: function(){
-    this.game.keyManager.update();
-    var player;
-    for(var ii=0;ii<this.players.length;ii+=1){
-      player = this.players[ii];
+  updateState: function(player){
+    if(player.state == "chewing"){
       if(this.game.keyManager.isReleased(player.leftKey) || this.game.keyManager.isReleased(player.rightKey)){
         player.chewSpeed+=10;
       }
       player.chewValue += player.chewSpeed;
       player.chewSpeed -= 1;
       if(player.chewSpeed <= 0){ player.chewSpeed = 0; }
-      this.updateStatText(player);
+
+      if(player.chewValue > player.optimalChewMax){
+        player.progressText.text = this.messages.chewOver;
+      } else if(player.chewValue > (player.optimalChewMax - (player.optimalChewMax - player.optimalChewMin)/3)){
+        if(player.chewSpeed > 0){
+          player.progressText.text = this.messages.chewAlmostOver;
+        } else {
+          player.progressText.text = this.messages.chewAchieved;
+        }
+      } else if(player.chewValue > player.optimalChewMin){
+        player.progressText.text = this.messages.chewAchieved;
+      } else if(player.chewValue > player.optimalChewMin/2){
+        player.progressText.text = this.messages.chewAlmostAchieved;
+      } else if(player.chewValue > 0){
+        player.progressText.text = this.messages.chewContinue;
+      } else {
+        player.progressText.text = this.messages.chewStart;
+      }
+      player.progressText.x = player.left+player.width/2-player.progressText.width/2;
+
+      if(player.chewValue > player.optimalChewMin && player.chewSpeed == 0){
+        player.state = "doneChewing";
+      }
+    } 
+    if(player.state == "doneChewing"){
+      if(player.chewValue > player.optimalChewMax){
+        this.floatText(player, "YEUGH!");
+      }else{
+        this.floatText(player, "TENDER!");
+      }
+      player.directionText.text = this.messages.blowDirection(player);
+      player.directionText.x = player.left+player.width/2-player.directionText.width/2;
+      player.progressText.text = this.messages.blowStart;
+      player.progressText.x = player.left+player.width/2-player.progressText.width/2;
+      player.state = "blowing";
+    }
+    if(player.state == "blowing"){
+      "!";
+    }
+    this.updateStatText(player);
+  },
+  floatText: function(player, words){
+    var text = this.game.add.text(
+      0, player.character.top-30,
+      words,
+      { fill: '#000', fontSize: 12 }
+    )
+    text.x = player.left+player.width/2-text.width/2;
+    text.rotation = -0.1;
+    var tween = this.game.add.tween(text).to( { alpha: 0.0, y: text.y - 60 }, 1000, Phaser.Easing.Default, true);
+    tween.onComplete.add(function(){text.destroy();})
+  },
+  update: function(){
+    this.game.keyManager.update();
+    var player;
+    for(var ii=0;ii<this.players.length;ii+=1){
+      player = this.players[ii];
+      this.updateState(player);
+    }
+    if(this.game.keyManager.isReleased('enter')){
+      player.state = "doneChewing";
     }
   }
 }
